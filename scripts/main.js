43a0d401a5714e3ffc47e26756a4018f59c4fd49
Ivy <vyvymiao1217@outlook.com>
2016-03-05 14:20:53 -0500

    more pages built

--- scripts/main.js
+++ scripts/main.js
@@ -8,16 +8,32 @@
 	current = current.split('/');
 	current = current[1];
 	current = current.replace(/work-landing/, 'work');
-	console.log(current);
+	// console.log(current);
 	$('.sc-header__navigation__link').each(function () {
 		var $this = $(this);
 		var href = $this.attr('href');
 		href = href.replace(/\//,'');
 		href = href.replace(/work-landing/, 'work');
-		console.log(href);
+		// console.log(href);
 		if (current === href) {
 			$this.addClass('is-checked');
 		}
+	});
+	$('#about').on('click', function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+	});
+	$('#contact').on('click', function (){
+		window.location.href='/contact';
+	});
+	$('#video1').on('click', function (){
+		window.open('https://vimeo.com/140318466');
+	});
+	$('#video2').on('click', function (){
+		window.open('http://houseandhome.com/video/small-yet-glamorous-condo/');
+	});
+	$('#video3').on('click', function (){
+		window.open('http://houseandhome.com/video/stacey-cohens-cocktail-lounge/');
 	});
 
 })();
--- scripts/work-individual.js
+++ scripts/work-individual.js
@@ -3,6 +3,22 @@
  */
 
 (function () {
-	'use strict';
-	
+	$('.back').on('click', function () {
+		window.location.href='/work/#filter=*';
+	});
+	$('.view-more').on('click', function() {
+		$('.sc-work-modal a:first').click();
+	});
+	var current = window.location.href;
+	current = current.replace(/http:\/\//, "");
+	current = current.split('/');
+	var name = current[2];
+	lightbox.option({
+		'resizeDuration': 200,
+		'albumLabel': name + ': image %1 / %2',
+		'disableScrolling': true,
+		'positionFromTop': 50,
+		'maxHeight': 800,
+		'maxWidth': 1000
+    });
 })();
